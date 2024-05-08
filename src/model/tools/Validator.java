ce0d51243f55452c57e5673dee994c25f9fc344e
F-Farhanpoor <farhang.farhanpoor@gmail.com>
2024-05-08 09:13:04 +0330

    Initial commit

---
+++ src/model/tools/Validator.java
@@ -0,0 +1,17 @@
+package model.tools;
+
+import controller.exception.DataValidationException;
+
+import java.util.regex.Pattern;
+
+public class Validator {
+    public static String validateName(String name, String message) throws DataValidationException {
+
+        if (Pattern.matches("^[a-zA-Z\\s]{2,30}$", name)){
+            return name;
+        }else{
+            throw new DataValidationException(message);
+        }
+    }
+
+}
